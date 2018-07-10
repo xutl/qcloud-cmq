bf95c98585d9b77db1d5daec58283d8e9392f9d6
Xu Tongle <xutongle@gmail.com>
2018-07-10 11:20:55 +0800

    init

---
+++ src/Client.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace XuTL\QCloud\Cmq;
+
+/**
+ * Class Client
+ * @package XuTL\QCloud\Cmq
+ */
+class Client
+{
+}
