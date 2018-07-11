aa50ee91c726fec0d18225587a7cc2cdb0737e95
Xu Tongle <xutongle@gmail.com>
2018-07-11 11:17:31 +0800

    update

--- src/Responses/ListQueueResponse.php
+++ src/Responses/ListQueueResponse.php
@@ -15,7 +15,48 @@ use XuTL\QCloud\Cmq\Http\BaseResponse;
  */
 class ListQueueResponse extends BaseResponse
 {
+    /**
+     * @var int
+     */
     public $totalCount;
 
+    /**
+     * @var array
+     */
     public $queueList;
+
+    /**
+     * 获取队列列表
+     * @return array
+     */
+    public function getQueueList()
+    {
+        return $this->queueList;
+    }
+
+    /**
+     * 获取队列Id
+     * @return array
+     */
+    public function getQueueIds()
+    {
+        $queueIds = [];
+        foreach ($this->queueList as $queue) {
+            $queueIds[] = $queue['queueId'];
+        }
+        return $queueIds;
+    }
+
+    /**
+     * 获取队列名称
+     * @return array
+     */
+    public function getQueueNames()
+    {
+        $queueNames = [];
+        foreach ($this->queueList as $queue) {
+            $queueNames[] = $queue['queueName'];
+        }
+        return $queueNames;
+    }
 }
--- src/Responses/ListTopicResponse.php
+++ src/Responses/ListTopicResponse.php
@@ -7,14 +7,56 @@
 
 namespace XuTL\QCloud\Cmq\Responses;
 
-
 use XuTL\QCloud\Cmq\Http\BaseResponse;
 
+/**
+ * Class ListTopicResponse
+ * @package XuTL\QCloud\Cmq\Responses
+ */
 class ListTopicResponse extends BaseResponse
 {
+    /**
+     * @var int
+     */
     public $totalCount;
 
+    /**
+     * @var array
+     */
     public $topicList;
 
+    /**
+     * 获取主题列表
+     * @return array
+     */
+    public function getTopicList()
+    {
+        return $this->topicList;
+    }
 
+    /**
+     * 获取队列Id
+     * @return array
+     */
+    public function getTopicIds()
+    {
+        $topicIds = [];
+        foreach ($this->topicList as $topic) {
+            $topicIds[] = $topic['topicId'];
+        }
+        return $topicIds;
+    }
+
+    /**
+     * 获取队列名称
+     * @return array
+     */
+    public function getTopicNames()
+    {
+        $topicNames = [];
+        foreach ($this->topicList as $topic) {
+            $topicNames[] = $topic['topicName'];
+        }
+        return $topicNames;
+    }
 }
